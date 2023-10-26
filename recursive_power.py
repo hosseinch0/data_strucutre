5cd694128b72a96fd64a3f93767e10383b00625b
Hossein Chahabdar <119678031+hosseinch0@users.noreply.github.com>
2023-10-26 22:23:43 +0330

    Update recursive_power.py

--- recursive_power.py
+++ recursive_power.py
@@ -1,12 +1,5 @@
-base = int(input("Insert the base: "))
-power = int(input("Insert the power: "))
-
-
-def recursive_power(base, power):
+def recursive_power(base=2, power=3):
     if power == 0:
         return 1
     else:
         return base * recursive_power(base, power - 1)
-
-
-print(recursive_power(base, power))
